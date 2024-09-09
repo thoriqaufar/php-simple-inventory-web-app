66026bc15502d5964516b5c732e4ad9ba6f2d0e1
Thoriq Aufar <thoriqaufarnubli@gmail.com>
2024-09-09 15:12:59 +0700

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\View\View;
+
+class ProductController extends Controller
+{
+    public function index(): View
+    {
+        $products = Product::latest()->paginate(10);
+
+        return view('products.index', compact('products'));
+    }
+
+    public function create(): View
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'image'         => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        $image = $request->file('image');
+        $image->storeAs('public/products', $image->hashName());
+
+        Product::create([
+            'image'         => $image->hashName(),
+            'title'         => $request->title,
+            'description'   => $request->description,
+            'price'         => $request->price,
+            'stock'         => $request->stock
+        ]);
+
+        return redirect()->route('products.index')->with(['success' => 'Product created successfully!']);
+    }
+
+    public function show(string $id): View
+    {
+        $product = Product::findOrFail($id);
+
+        return view('products.show', compact('product'));
+    }
+
+    public function edit(string $id): View
+    {
+        $product = Product::findOrFail($id);
+
+        return view('products.edit', compact('product'));
+    }
+
+    public function update(Request $request, $id): RedirectResponse
+    {
+        $request->validate([
+            'image'         => 'image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        $product = Product::findOrFail($id);
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $image->storeAs('public/products', $image->hashName());
+
+            Storage::delete('public/products/' . $product->image);
+
+            $product->update([
+                'image'         => $image->hashName(),
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+        } else {
+            $product->update([
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+        }
+
+        return redirect()->route('products.index')->with(['success' => 'Product updated successfully!']);
+    }
+
+    public function destroy($id): RedirectResponse
+    {
+        $product = Product::findOrFail($id);
+
+        Storage::delete('public/products/' . $product->image);
+
+        $product->delete();
+
+        return redirect()->route('products.index')->with(['success' => 'Product deleted successfully!']);
+    }
+}
